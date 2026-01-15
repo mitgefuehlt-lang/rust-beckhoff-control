e102ee5349b1e4337e275ff2d53f4a7992a04f0c
mitgefuehlt-lang <mitgefuehlt@gmail.com>
2026-01-15 13:47:15 +0100

    Fix: Restore flake.nix with crane and system config

--- flake.nix
+++ flake.nix
@@ -2,16 +2,44 @@
   description = "QiTech Rust Control Production";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
+    crane = {
+      url = "github:ipetkov/crane";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, crane, rust-overlay, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        craneLib = crane.mkLib pkgs;
+
+        # Server Package
+        server = pkgs.callPackage ./nixos/packages/server.nix {
+          inherit craneLib;
+          libudev-zero = pkgs.libudev-zero; # Ensure this exists or use systemd
+          libpcap = pkgs.libpcap;
+        };
+
+        # Electron Package
+        electron = pkgs.callPackage ./nixos/packages/electron.nix { };
+
       in
       {
+        packages = {
+          inherit server electron;
+          default = server;
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             cargo
@@ -22,11 +50,28 @@
             pkg-config
             udev
             openssl
+            nodejs
+            python3
           ];
           
-          # Set Environment Variables if needed
           RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
         };
       }
-    );
+    ) // {
+      # System Configuration
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixos/os/configuration.nix
+          ./nixos/modules/qitech.nix
+          ({ pkgs, ... }: {
+            nixpkgs.overlays = [
+              (final: prev: {
+                qitechPackages = self.packages.x86_64-linux;
+              })
+            ];
+          })
+        ];
+      };
+    };
 }

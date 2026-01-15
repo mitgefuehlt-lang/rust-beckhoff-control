0b68a808cfdf4288f0d9393e9811647966c1c01b
mitgefuehlt-lang <mitgefuehlt@gmail.com>
2026-01-15 14:09:06 +0100

    Fix: Track Cargo.lock and use nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "QiTech Rust Control Production";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     crane = {
       url = "github:ipetkov/crane";

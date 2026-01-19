d292f5684527babe7bd2a2d5afca0c5357618834
mitgefuehlt-lang <mitgefuehlt@gmail.com>
2026-01-19 15:51:05 +0100

    Fix: Broaden library paths and add deep diagnostics for libudev

--- nixos/modules/qitech.nix
+++ nixos/modules/qitech.nix
@@ -125,6 +125,7 @@ in {
             pkgs.zlib
             pkgs.libpcap
             pkgs.udev
+            pkgs.systemd
             pkgs.systemd.lib
             pkgs.glib
             pkgs.libudev-zero
